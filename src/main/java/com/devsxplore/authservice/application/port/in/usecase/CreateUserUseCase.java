7971972b30d2afa11d5718378a5a8a7f9608d2e4
Nawied Khaleqi <74689002+nawiedk@users.noreply.github.com>
2025-06-28 03:17:10 +0200

    remove service and usecases

--- src/main/java/com/devsxplore/authservice/application/port/in/usecase/CreateUserUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.devsxplore.authservice.application.port.in.usecase;
-
-import com.devsxplore.authservice.application.port.in.command.CreateUserCommand;
-import com.devsxplore.authservice.domain.model.User;
-
-public interface CreateUserUseCase {
-    User create(CreateUserCommand command);
-}
--- src/main/java/com/devsxplore/authservice/application/port/in/usecase/DeleteUserUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.devsxplore.authservice.application.port.in.usecase;
-
-import com.devsxplore.authservice.application.port.in.command.DeleteUserCommand;
-
-public interface DeleteUserUseCase {
-    void deleteUserByUserId(DeleteUserCommand command);
-}
